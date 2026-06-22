974db487c7b52ac7d48ac3fec1d1af23434e893e
Ian Luiz de Souza Bello <ian.bello@globalsys.com.br>
2026-06-22 15:50:02 -0300

    mudanças no .gitignore

--- TERRAFORM-COMPARTMENT/provider.tf
+++ TERRAFORM-COMPARTMENT/provider.tf
@@ -11,11 +11,6 @@ terraform{
         key = "TERRAFORM-COMPARTMENT/terraform.tfstate"
         namespace = "grgxeup22eul"
         region = "sa-saopaulo-1"
-
-        tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaane6guqzdfn4a7mf2uozeebkcummsbicm4lzccrqmijsbhzvm6oga"
-        user_ocid = "ocid1.user.oc1..aaaaaaaaskoxhq6j6iqmfkzhaq4alhwpwflssrwdxgqg2uwmomyyvxfir37a"
-        fingerprint = "ba:2f:cc:10:e2:71:28:6b:92:a0:2f:3f:f4:c1:21:fd"
-        private_key_path = "C:/Users/ian.bello/Documents/KEYS/API_KEY/oci_api_key.pem"
     }
 }
 
